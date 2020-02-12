02c47a9477aa058d9b3c18bf8e7c0cb9d5dc88ec
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-11 19:03:53 -0800

    Started ruby practice

--- day-1.rb
+++ day-1.rb
@@ -17,3 +17,26 @@
 # puts add("dog", "cat") == "dogcat"
 # puts add("callie", "squeaky") == "calliesqueaky"
 #
+
+# use .times and add (written above)
+def multiply(x, y)
+  x * y
+  end
+
+  variable
+end
+
+puts "MULTIPLY TESTS"
+puts multiply(1,1) == 1
+puts multiply(1, 2) == 2
+puts multiply(2, 3) == 6
+puts multiply(3, 4) == 12
+#
+# # use only .times and multiply (written above)
+# def power(x, y)
+# end
+#
+# puts "POWER TESTS"
+# puts power(1, 2) == 1
+# puts power(2, 3) == 8
+# puts power(3, 4) == 81
