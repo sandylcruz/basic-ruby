b37f9b4d98ea8cdc674b78b3ae6e2b77f0c300b0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-11 19:01:17 -0800

    Initialize ruby basic project

---
+++ day-1.rb
@@ -0,0 +1,19 @@
+# def add(x, y)
+#   x + y
+# end
+#
+# puts "ADD TESTS"
+# puts add(1, 2) == 3
+# puts add(2, 3) == 5
+# puts add(3, 4) == 7
+#
+# def concat(x, y)
+#   "catdog"
+#   x + y
+# end
+#
+# puts "CONCAT TESTS"
+# puts add("cat", "dog") == "catdog"
+# puts add("dog", "cat") == "dogcat"
+# puts add("callie", "squeaky") == "calliesqueaky"
+#
