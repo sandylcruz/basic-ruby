a0ce10ad60b440baa4f6a3a78b323ed0c077b0cf
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-14 21:33:07 -0800

    Practice while loops

---
+++ day-3.rb
@@ -0,0 +1,29 @@
+n = 0
+
+while n < 10
+  puts n
+  n += 1
+end
+
+bottle = 0
+until bottle == 10
+  bottle += 1
+end
+
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from? "
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp
+state.upcase!
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
