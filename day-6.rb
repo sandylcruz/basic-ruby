26308933a06851f1fffbda8764b8ce1bfe3eb0eb
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-16 18:50:55 -0800

    Practice ruby fundamentals

---
+++ day-6.rb
@@ -0,0 +1,25 @@
+# def say_hi
+#   puts "Hi squeaky"
+# end
+#
+# say_hi
+
+# def clap_hands(number)
+#   puts "Clap" * number
+# end
+#
+# clap_hands(10)
+
+# def square(number)
+#   number ** 2
+# end
+#
+# puts square(5)
+
+# first_name = "squeaky"
+# last_name = "cat"
+# puts "#{first_name} #{last_name}"
+
+# puts "#{first_name}" + "#{last_name}"
+
+{:dog => 'barks', :cat => 'meows', :pig => 'oinks'}[:cat]
