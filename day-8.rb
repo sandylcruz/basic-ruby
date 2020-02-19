65404a7f8826318c275f187b54b3e29fe8f8b5b9
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-18 18:39:55 -0800

    Continue ruby method practice

---
+++ day-8.rb
@@ -0,0 +1,35 @@
+# def reverse(array)
+#   array = [1, 2, 3, 4, 5]
+#
+# while i <
+#
+#
+#   array
+#
+# end
+#
+# .unshift
+# puts reverse([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]
+
+# def greeting(name)
+#   "Hi #{name} how are you doing?"
+# end
+#
+# puts greeting("Callie")
+
+# def multiply(number1, number2)
+#   number1 * number2
+# end
+#
+# puts multiply(4, 2)
+
+puts "Put in a number"
+a = gets.chomp.to_i
+
+if a == 3
+  puts "a is 3"
+elsif a == 4
+  puts "a is 4"
+else
+  puts "a is neither 3, nor 4"
+end
