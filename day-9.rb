4c97637b99d4814eaeacc85b35e9964ed893ba96
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-19 18:59:14 -0800

    Create and manipulate arrays

---
+++ day-9.rb
@@ -0,0 +1,46 @@
+# number = 1
+#
+# def add_to(number)
+#   number + 2
+# end
+
+# array = ["hi", "foo", "bar"]
+# puts array[0]
+# puts array[1]
+
+# puts [1, 2, 3, 4, 5].size
+
+# array = [1, 2, 3]
+#   puts array << 4
+#
+# array = [5, 6, 7]
+#   puts array << 8
+#
+# array = [11, 12, 13]
+#   puts array << 14, 15
+
+# array = ["hi", "callie", "cat"]
+#   puts array << "squeaky"
+#   puts array [3, 4]
+
+# array = ["one", "two", "three"]
+#   puts array << "cats"
+#   puts array [0]
+
+# array = [11, 12, 13]
+#   puts array << [4]
+#   puts array [1]
+
+# array = ["hello", "I", "am", "Callie"]
+#   puts array << "cat"
+#   puts array [1]
+
+# array = [13, 14, 15]
+#   puts array << 17
+#   puts array [2]
+
+# array = [1, 2, 3, 4]
+#   puts array.delete_at(2)
+
+array = [1, 2, 3]
+  puts array << 16
