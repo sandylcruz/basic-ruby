9dbc079c88369594aa7442bf7cf72b9fbe91f7e3
Sandy Cruz <sandyleahcruz@icloud.com>
2020-02-27 18:34:19 -0800

    Test array and method skills

---
+++ day-17.rb
@@ -0,0 +1,123 @@
+# word = "lace"
+# word[0] = "m"
+# puts word
+#
+# word = "cat"
+# word[2] = "r"
+# puts word
+#
+# word = "callie"
+# word[0] = "t"
+# puts word
+#
+# word = "katerina"
+# word[1] = "e"
+# puts word
+#
+# word = "cally"
+# word[4] = "ie"
+# puts word
+#
+# word = "dog"
+# word[1] = "i"
+# puts word
+#
+# word = "dress"
+# word[2] = "y"
+# puts word
+
+# word = "kitten"
+# word[5] = "y"
+# puts word
+
+# my_array = [1, 2]
+# my_array = []
+# array = ["mike tyson", 89, 9.7]
+#
+# ruff = ["stop", "drop", "shut 'em down"]
+# ruff << "open up shop"
+# puts ruff
+
+# cats = ["squeaky"]
+# cats << "callie"
+# puts cats
+
+# cats = ["callie"]
+# cats << "squeaky"
+# puts cats
+
+# if 5
+#   puts "Hello there"
+# end
+#
+# if nil
+#   puts "nil is truthy"
+# else
+#   puts "nil is falsey"
+# end
+#
+# puts "This syntax is cool"
+#   if true
+#
+# puts "Tall buildings" if false
+#
+# mode = "chill"
+# mode += "hippie"
+#
+# mode = "chill"
+# puts mode = mode + "hippie"
+
+# counter = 0
+# while counter < 3
+#   puts "Went through the loop"
+#   counter = counter + 1
+# end
+#
+# def my_name()
+#   return("Zoo Lander")
+# end
+
+# def my_name()
+#   return("Zoo Lander")
+# end
+# puts my_name()
+
+# def my_name()
+#   return ("Callie Cat")
+# end
+# puts my_name()
+#
+# def my_name()
+#   return("Squeaky Squeaks")
+# end
+# puts my_name
+#
+# def my_name
+#   return ("Katerina Kitten")
+# end
+# puts my_name
+
+# def my_name()
+#   return("Squeaky cat")
+# end
+# puts my_name()
+
+# def add(x, y)
+#   return (x+y)
+# end
+# add(3,5)
+
+# def multiply(x, y)
+#   return (x * y)
+# end
+# puts multiply(5,3)
+
+# def subtract(x,y)
+#   return (x - y)
+# end
+# puts subtract(5,1)
+#
+# def divide(x,y)
+#   return (x / y)
+# end
+# puts divide(15,5)
