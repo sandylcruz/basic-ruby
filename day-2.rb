6e80c9585b48331a82f41103eb90537ad1ba93ae
Sandy Cruz <sandyleahcruz@icloud.com>
2020-03-11 18:42:24 -0700

    Fix method errors

--- day-2.rb
+++ day-2.rb
@@ -1,27 +1,38 @@
 #basic loops
 
-x = gets.chomp.to_i
-
-until x < 0
-  puts x
-  x -= 1
-end
-
-puts "Done!"
-
-stock = 10
-
-if stock <1
-  puts "Sorry we're out of stock"
-else
-  puts "Thanks for your order"
-end
-
-3.times {puts "squeaky"}
-
-n = 0
-
-while n<10
-  puts n
-   n+=1
+def countdown(x)
+  until x < 0
+    puts x
+    x -= 1
   end
+  puts "Done!"
+end
+
+puts countdown(9)
+
+def stock_check(n)
+  stock = n
+  if stock <1
+    puts "Sorry we're out of stock"
+  else
+    puts "Thanks for your order"
+  end
+end
+
+stock_check(0)
+
+def squeaky(x)
+  x.times do
+    puts "squeaky"
+  end
+end
+squeaky(5)
+
+def count_to_ten(n)
+  while n<10
+    puts n
+    n += 1
+  end
+end
+
+count_to_ten(4)
