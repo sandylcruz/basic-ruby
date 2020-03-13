88d7f5fa2a38248c417b1f3c671ad5704b063b0e
Sandy Cruz <sandyleahcruz@icloud.com>
2020-03-12 19:31:15 -0700

    Edit loop

--- day-3.rb
+++ day-3.rb
@@ -1,29 +1,36 @@
-n = 0
+#
+# def count_up_to_nine(n)
+#   while n <= 10
+#     puts n
+#     n += 1
+#   end
+# end
+#
+# count_up_to_nine(4)
 
-while n < 10
-  puts n
-  n += 1
-end
+# def bottle(n)
+#   until n == 10
+#     n += 1
+#   end
+# end
+#
+# puts bottle(4)
 
-bottle = 0
-until bottle == 10
-  bottle += 1
-end
 
-print "What's your first name? "
-first_name = gets.chomp
-first_name.capitalize!
+  print "What's your first name? "
+  first_name = gets.chomp
+  first_name.capitalize!
 
-print "What's your last name? "
-last_name = gets.chomp
-last_name.capitalize!
+  print "What's your last name? "
+  last_name = gets.chomp
+  last_name.capitalize!
 
-print "What city are you from? "
-city = gets.chomp
-city.capitalize!
+  print "What city are you from? "
+  city = gets.chomp
+  city.capitalize!
 
-print "What state or province are you from? "
-state = gets.chomp
-state.upcase!
+  print "What state or province are you from? "
+  state = gets.chomp
+  state.upcase!
 
-puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+  puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
