d3fb95868394c17b2ba327d99906bec10a5b5241
Sandy Cruz <sandyleahcruz@icloud.com>
2020-03-13 18:39:43 -0700

    Edit while loop

--- day-4.rb
+++ day-4.rb
@@ -1,11 +1,8 @@
-# a = [0,1,2,3]
-# puts a
-
-x = 0
-y = 0
-while x < 4 && y < 4
-
-   puts "(#{x}, #{y})"
-   x += 1
-   y += 1
+def numbers(x,y)
+  while x < 4 && y < 4
+     puts "(#{x}, #{y})"
+     x += 1
+     y += 1
+  end
 end
+puts numbers(3,1)
