83cc7d6a37cba57c10e9672ed9beefd1aaa8ed49
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-28 11:47:04 -0700

    Practice using classes

---
+++ day-100.rb
@@ -0,0 +1,40 @@
+class MyCar
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def current_speed
+    "Our current speed is #{@current_speed} miles per hour"
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    "We have sped up by #{number} miles per hour"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    "We are braking. We have slowed down by #{number} miles per hour"
+  end
+
+  def shut_down
+    shut_off = 0
+    "We have shut off the car. We are going 0 miles per hour."
+  end
+
+end
+puts lumina = MyCar.new(1997, 'chevy lumina', 'white')
+puts lumina.speed_up(20)
+puts lumina.current_speed
+puts lumina.speed_up(20)
+puts lumina.current_speed
+puts lumina.brake(20)
+puts lumina.current_speed
+puts lumina.brake(20)
+puts lumina.current_speed
+puts lumina.shut_down
+puts lumina.current_speed
